a1581296385adb0c51287f5c5f29f0ad87f1b8b3
Pop Catalin <pop.catalin@gmail.com>
2020-05-15 12:59:58 +0300

    Add Geco project
    Move Project out of .Tools folder
    
    Update solution to use Geco.Core submodule
    
    Changed Default generate directories
    
    Update to .Net Core 3.1

---
+++ Geco/Program.cs
@@ -0,0 +1,288 @@
+﻿// Copyright © iQuarc 2017 - Pop Catalin Sever
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using Geco.Common;
+using Geco.Common.MetadataProviders.SqlServer;
+using Geco.Common.SimpleMetadata;
+using Geco.Config;
+using Microsoft.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using static Geco.Common.ColorConsole;
+using static System.ConsoleColor;
+
+namespace Geco
+{
+    /// <summary>
+    /// As simple as it gets code generator, which is a console application that runs code generation tasks written in C#.
+    /// </summary>
+    /// <remarks>
+    /// Task discovery is done at runtime by scanning current assembly for all the types that implement <see cref="IRunnable"/> interfaces.
+    /// The tasks are resolved using a <see cref="ServiceProvider"/>. Generator tasks can declare a options class using the <see cref="OptionsAttribute"/>
+    /// in order to have the options be read from the <c>appsettings.json</c> configuration file and registered in the <see cref="Microsoft.Extensions.DependencyInjection.ServiceCollection"/>
+    /// </remarks>
+    public class Program
+    {
+        private Dictionary<string, Type> runnableTypes;
+        private IServiceCollection serviceCollection;
+        private RootConfig rootConfig;
+        private IConfigurationRoot configurationRoot;
+        private bool Interactive { get; set; }
+        static int Main(string[] args)
+        {
+            var p = new Program();
+            return p.Run(args); 
+        }
+
+        private int Run(string[] args)
+        {
+            try
+            {
+                Directory.SetCurrentDirectory(AppDomain.CurrentDomain.BaseDirectory);
+
+                var app = new CommandLineApplication();
+                app.Name = "Geco";
+                app.HelpOption("-?|-h|--help");
+
+                app.Command("run", command =>
+                {
+                    command.HelpOption("-?|-h|--help");
+                    var taskList = command.Option("-tl|--tasklist "
+                        , "The name of the task list from appsettings.json to execute. The task list is an array of task names.", CommandOptionType.SingleValue);
+                    var taskNames = command.Option("-tn|--taskname <taskname>"
+                        , "The name(s) of the tasks to execute. The task names is an list of task names parameters -tn <xx> -tn <yy>.", CommandOptionType.MultipleValue);
+                    command.OnExecute(() =>
+                    {
+                        ConfigureServices(app.RemainingArguments.ToArray());
+                        if (taskList.HasValue())
+                            RunTaskListFromConfig(taskList.Value());
+                        if (taskNames.HasValue())
+                            RunTasksList(taskNames.Values);
+                        return 0;
+                    });
+                });
+                app.OnExecute(() =>
+                {
+                    WriteLogo();
+                    ConfigureServices(app.RemainingArguments.ToArray());
+                    WriteLine($"<< Geco is running in interactive mode! >>", Yellow);
+                    InteractiveLoop();
+                    WriteLine($"C ya!", Yellow);
+                    return 0;
+                });
+
+                return app.Execute(args);
+            }
+            catch (Exception ex)
+            {
+                WriteLine($"==============================================", Red);
+                WriteLine($"=== Geco stopped due to error:", Red);
+                WriteLine($"{ex}", Yellow);
+                WriteLine($"==============================================", Red);
+                return -1;
+            }
+        }
+
+        private void InteractiveLoop()
+        {
+            Interactive = true;
+            Func<bool> displayAndRun;
+            do
+            {
+                displayAndRun = BuildMenu();
+            } while (displayAndRun());
+        }
+
+        private Func<bool> BuildMenu()
+        {
+            Console.WriteLine();
+            WriteLine($"Select option {("(then press Enter)", Gray)}:", White);
+            var actions = new Dictionary<string, Action>();
+            foreach (var taskInfo in rootConfig.Tasks.WithInfo())
+            {
+                var taskNr = (taskInfo.Index + 1).ToString();
+                WriteLine(($"{taskNr}. ", White), ($"{taskInfo.Item.Name}", Blue));
+                actions.Add(taskNr, () => RunTask(taskInfo.Item));
+            }
+            WriteLine(("q. ", White), ("Quit", ConsoleColor.Yellow));
+            Write($">>", White);
+
+            bool Choose()
+            {
+                var command = Console.ReadLine();
+                if (string.Equals(command, "q", StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine();
+                    return false;
+                }
+                if (actions.TryGetValue(command, out var action))
+                    action();
+                return true;
+            }
+            return Choose;
+        }
+
+        private void ConfigureServices(string[] args)
+        {
+            configurationRoot = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .AddCommandLine(args)
+                .Build();
+
+            //setup the DI
+            serviceCollection = new ServiceCollection()
+                .AddLogging()
+                .AddSingleton(configurationRoot)
+                .AddOptions()
+                .AddSingleton<IMetadataProvider, SqlServerMetadataProvider>()
+                .AddSingleton<IInflector, HumanizerInflector>();
+
+            ScanTasks();
+        }
+
+        private static void WriteLogo()
+        {
+            var version = Assembly.GetEntryAssembly().GetName().Version;
+            WriteLine($"********************************************************", Blue);
+            WriteLine($"* ** Geco v{version} **                                  *", Blue);
+            WriteLine($"*                                                      *", Blue);
+            WriteLine(("*", Blue), (@"        .)/     )/,         ", Green), ("        Copyright (c)     *", Blue));
+            WriteLine(("*", Blue), (@"         /`-._,-'`._,@`-,   ", Green), ("         iQuarc 2017      *", Blue));
+            WriteLine(("*", Blue), (@"  ,  _,-=\,-.__,-.-.__@/    ", Green), ("    - Generator Console - *", Blue));
+            WriteLine(("*", Blue), (@" (_,'    )\`    '(`         ", Green), ("           - Geco -       *", Blue));
+            WriteLine($"*                                                      *", Blue);
+            WriteLine($"*          {("https://github.com/iQuarc/Geco.Core", DarkMagenta)}         *", Blue);
+            WriteLine($"********************************************************", Blue);
+        }
+
+        private void ScanTasks()
+        {
+            rootConfig = new RootConfig();
+            configurationRoot.Bind(rootConfig);
+
+            runnableTypes = Assembly.GetAssembly(typeof(Program))
+                .GetTypes()
+                .Where(t => typeof(IRunnable).IsAssignableFrom(t))
+                .Where(t => !t.IsAbstract && !t.IsGenericTypeDefinition && t.GetConstructors().Any())
+                .ToDictionary(t => t.FullName);
+
+            foreach (var runnableType in runnableTypes.Values)
+            {
+                serviceCollection.Add(new ServiceDescriptor(runnableType, runnableType, ServiceLifetime.Transient));
+            }
+
+            //RootConfig rootConfig
+            foreach (var taskConfig in rootConfig.Tasks.WithInfo())
+            {
+                if (!runnableTypes.ContainsKey(taskConfig.Item.TaskClass))
+                {
+                    WriteLine($"Task configuration for:[{taskConfig.Item.TaskClass}] has no corresponding service to be applied to", DarkYellow);
+                    continue;
+                }
+                var taskType = runnableTypes[taskConfig.Item.TaskClass];
+                var optionsAttribute = (OptionsAttribute)taskType.GetCustomAttribute(typeof(OptionsAttribute));
+                if (optionsAttribute != null)
+                {
+                    taskConfig.Item.ConfigIndex = taskConfig.Index;
+                    var options = Activator.CreateInstance(optionsAttribute.OptionType);
+                    configurationRoot.GetSection($"Tasks:{taskConfig.Item.ConfigIndex}:Options").Bind(options);
+                    serviceCollection.Replace(new ServiceDescriptor(optionsAttribute.OptionType, options));
+                }
+            }
+        }
+
+        private void RunTaskListFromConfig(string taskListName)
+        {
+            var taskList = new List<string>();
+            configurationRoot.Bind(taskListName, taskList);
+            RunTasksList(taskList);
+        }
+
+        private void RunTasksList(IEnumerable<string> taskList)
+        {
+            foreach (var taskName in taskList)
+            {
+                var task = rootConfig.Tasks.Find(t => t.Name == taskName);
+                task.OutputToConsole = false;
+                RunTask(task);
+            }
+        }
+
+        private void RunTask(TaskConfig itemInfo)
+        {
+            var sw = new Stopwatch();
+            try
+            {
+                WriteLine($"--------------------------------------------------------", Yellow);
+                WriteLine(("*** Starting ", Yellow), ($" {itemInfo.Name} ", Blue));
+
+                var taskType = runnableTypes[itemInfo.TaskClass];
+                var optionsAttribute = (OptionsAttribute)taskType.GetCustomAttribute(typeof(OptionsAttribute));
+                if (optionsAttribute != null)
+                {
+                    var options = Activator.CreateInstance(optionsAttribute.OptionType);
+                    configurationRoot.GetSection($"Tasks:{itemInfo.ConfigIndex}:Options").Bind(options);
+                    serviceCollection.Replace(new ServiceDescriptor(optionsAttribute.OptionType, options));
+                }
+
+                using (var provider = serviceCollection.BuildServiceProvider())
+                {
+                    sw.Start();
+                    var task = (IRunnable)provider.GetService(taskType);
+                    if (task is IOutputRunnable to)
+                    {
+                        to.OutputToConsole = itemInfo.OutputToConsole;
+                        to.BaseOutputPath = itemInfo.BaseOutputPath;
+                        to.CleanFilesPattern = itemInfo.CleanFilesPattern;
+                        to.Interactive = Interactive;
+                    }
+
+                    if (task is IRunableConfirmation co && Interactive)
+                    {
+                        sw.Stop();
+                        if (!co.GetUserConfirmation())
+                        {
+                            WriteLine(("*** Task was canceled ", Yellow), ($" {itemInfo.Name} ", Blue));
+                            return;
+                        }
+                        sw.Start();
+                    }
+                    try
+                    {
+                        task.Run();
+                    }
+                    catch (OperationCanceledException)
+                    {
+                        WriteLine(("*** Task was aborted ", Yellow), ($" {itemInfo.Name} ", Blue));
+                    }
+                }
+            }
+            catch (Exception ex) when (Interactive)
+            {
+                WriteLine($"Error running {(itemInfo.Name, Blue)}: Error:{(ex.Message, Red)}", DarkRed);
+                WriteLine($"Detail: {ex}", DarkYellow);
+            }
+            sw.Stop();
+            Console.WriteLine();
+            WriteLine(("Task", Yellow), ($" {itemInfo.Name} ", Blue), ("completed in", Yellow), ($" {sw.ElapsedMilliseconds} ms", Green));
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,217 @@
-# Geco.Core
-Core Repository for Geco, A Simple code generator based on a console project, running on .Net core and using C# interpolated strings. Format suitable to be used a s git submodule
+# Geco, (Ge)nerator (Co)nsole
+Simple code generator based on a console project, running on .Net Core and using C# interpolated strings.
+
+Geco runs on .Net Core 3.1. [Download and Install](http://dot.net)
+
+What's the reasoning behind this utility?
+
+One of the most popular code generation tools for Visual Studio over the years was T4 (Text Template Transformation Toolkit), which saw a great deal of success for simple to complex code generation tasks.
+Scott Hanselman has a nice [blog post](https://www.hanselman.com/blog/T4TextTemplateTransformationToolkitCodeGenerationBestKeptVisualStudioSecret.aspx) about it. However, since the advent of **.Net Core** and .Net going cross platform there is no simple way to generate code or other artifacts that would also work in Visual Studio Code (or other code editors).
+
+This is where the idea for this simple utility came from. Have a small utility for code generation that is **debuggable** *(one of the long standing issues of T4)*, provides some level of **intellisense**, can be run at build time, is higly customizable, generic and has an as small as possible footprint.
+
+## Installation
+
+Geco can be installed as a [Visual Studio Project Template](https://marketplace.visualstudio.com/items?itemName=catalinpop.Geco101) or as `dotnet new` template. 
+
+To install Geco as a `dotnet new template` run the following command on a console or terminal:
+
+```Batchfile
+dotnet new -i iQuarc.Geco.CSharp
+```
+
+Then create a new project using the newly installed template:
+
+```Batchfile  
+dotnet new geco
+```
+
+To create a Geco project when installed as a Visual Studio Project template, Select File -> New project select the Geco template. 
+
+### A more indetail Getting Sstarted with Visual Studio walkthrough can be found on the Wiki: [Geting Started](https://github.com/iQuarc/Geco/wiki)
+
+Geco can also be installed as a Git submodule. `Geco.Core` is the git repository which should be added. The recomendation is to add it to a folder called .Tools
+
+```Batchfile
+git submodule add https://github.com/iQuarc/Geco.Core.git .Tools
+```
+
+## Code Generation examples
+
+Bellow are some snippets from the included code generation tasks that are in the default Geco package.
+
+Next snippet is from the included Entity Framework Core Reverse model generator [EntityFrameworkCoreReverseModelGenerator.cs](https://github.com/iQuarc/Geco/blob/master/src/Geco/Database/EntityFrameworkCoreReverseModelGenerator.cs), which exemplifies part of the code generation:
+
+![Geco Preview1](https://github.com/iQuarc/Geco/blob/dev/dist/GecoResources/PreviewImage.JPG?raw=true)
+
+This snippet is from the SQL Seed Scrip Generator [SeedDataGenerator.cs](https://github.com/iQuarc/Geco/blob/master/src/Geco/Database/SeedDataGenerator.cs), which generates SQL Merge scripts for Seed data:
+
+![Geco Preview2](https://github.com/iQuarc/Geco/blob/dev/dist/GecoResources/PreviewImage2.JPG?raw=true)
+
+Next screen shot shows Geco running in interactive mode (as a Console App), and the menu that is presented to the user:
+
+![Geco Preview3](https://github.com/iQuarc/Geco/blob/dev/dist/GecoResources/PreviewImage3.JPG?raw=true)
+
+
+## Description
+
+Geco uses C# 6.0 string interpolation as a template engine for code generation, in order to allow:
+
+ - Easy customization of templates (Simply edit the .cs file)
+ - Easy debugging (Place a breakpoint an run)
+ - Easy extensibility (Add a new task by creating a new C# class and implement a simple interface `IRunnable`)
+ 
+Geco uses task discovery at runtime and each task is configured using Dependency Injection. The generation tasks can be run during build or from interactive mode (Debug Run).
+
+## Included generators
+
+The following generator tasks are included in current version.
+
+ - Entity Framework Core 1.1, 2.0 Reverse model generator
+ - SQL Seed data script generator (Generates MERGE scripts)
+ - SQL Script runner
+ - Database cleaner
+
+## Customizing
+
+1. The first customization option involves changing task parameters from `appsettings.json` configuration file. Each Geco task defines its own set of parameters (see the corresponding Options class):
+
+```JSon
+{
+    "ConnectionStrings": {
+        "DefaultConnection": "Integrated Security=True;Initial Catalog=AdventureWorks;Data Source=.\\SQLEXPRESS;"
+    },
+    "RunAtBuildTasks": [
+        "Generate EF Core model"
+    ],
+    "Tasks": [
+        {
+            "Name": "Generate EF Core model",
+            "TaskClass": "Geco.Database.EntityFrameworkCoreReverseModelGenerator",
+            "BaseOutputPath": "..\\..\\Generated\\Model\\",
+            "OutputToConsole": "false",
+            "CleanFilesPattern": "*.cs",
+            "Options": {
+                "ConnectionName": "DefaultConnection",
+                "Namespace": "Model",
+                "OneFilePerEntity": true,
+                "JsonSerialization": true,
+                "GenerateComments": true,
+                "UseSqlServer": true,
+                "ConfigureWarnings": true,
+                "GeneratedCodeAttribute": false,
+                "NetCore": true,
+                "ContextName":"AdventureworksContext"
+            }
+        }
+    ]
+}
+```
+
+2. By customizing existing templates directly.
+
+3. By creating new code generation tasks as a C# class which implements the `IRunnable` interface.
+
+`IRunnable` is a single method interface containing the `Run` method:
+
+ ```CSharp
+    /// <summary>
+    /// Represents a runable task, for code generation or purposes or others
+    /// </summary>
+    public interface IRunnable
+    {
+        /// <summary>
+        /// Invoked when this task is executed
+        /// </summary>
+        void Run();
+    }   
+ ```
+ or by deriving from one of the helper base classes `BaseGenerator` or `BaseGeneratorWithMetadata`.
+
+A code generation class can be accompanied by an Options POCO class to hold customization parameters from `appsettings.json`
+
+Example:
+
+```CSharp
+    public class SeedDataGeneratorOptions
+    {
+        public string ConnectionName { get; set; }
+        public string OutputFileName { get; set; }
+        public List<string> Tables { get; } = new List<string>();
+        public string TablesRegex { get; set; }
+        public List<string> ExcludedTables { get; } = new List<string>();
+        public string ExcludedTablesRegex { get; set; }
+        public int ItemsPerStatement { get; set; } = 1000;
+    }
+        /// <summary>
+    /// Generates seed scripts with merge statements for (Sql Server)
+    /// </summary>
+    [Options(typeof(SeedDataGeneratorOptions))]
+    public class SeedDataGenerator : BaseGeneratorWithMetadata
+    {
+    // ...
+    }
+    
+```
+## Roadmap
+
+ - ASP.Net MVC Template (scaffolds controllers and views)
+ - ASP.Net + Angular scaffolder (scaffolds Web Api and components)
+ - SQL Sever to SQL Compact data sync template (Api to synchronize schema compatible SQL Server and SQL Compact databases including Web Api and .Net client methods)
+
+## Version History
+
+**Version 1.0.9**
+  - Updated Geco to .Net Core 3.1
+
+**Version 1.0.8**
+  - Fix VS dependency version
+
+**Version 1.0.7**
+  - Add support for VS 2019
+  - Add support for Ignored columns in Seed Data generator (Sergiu Damian)
+  - Add support in Seed Data Generator to order rows by clustered Index (Sergiu Damian)
+
+**Version 1.0.6**
+  - Update to *netcoreapp2.1*
+
+**Version 1.0.5**
+  - Fixed pluralization bug
+  - Fixed issue with current directory, not being set to bin\
+  - Fixed nullable date time not being generated correctly
+
+**Version 1.0.4**
+  - Replaced `EnglishInflector` with Humanizer based version
+  - Improved metadata API to allow easier removal of tables and columns along with dependent objects
+  - Improved `SeedScriptRunner` error message when the seed file does not exist 
+
+**Version 1.0.3**
+  - Fix SqlServerMetadataProvider bug with global database triggers
+  - Added display of errors in interactive
+
+**Version 1.0.2**
+
+  - Added constraint delete behavior support
+  - Modified logo
+
+**Version 1.0.1**
+
+ - Changed Entity Framework Core generator to allow multi column foreign keys
+ - Fixed MetaData for MaxLength nvarchar and nchar columns
+ - Removed tables without primary key from generated model
+ - Switched to semver
+
+**Version 1.0.0.2**
+
+- Changed user confirmation mechanism
+- Added user confirmation for file cleaning
+- Added more options for color output to console, formattable strings with tuple (value, ConsoleColor) parameters
+
+**Version 1.0.0.1**
+
+- First release as a `nuget dotnet` new template
+
+**Version 1.0.0.0-beta**
+
+- Initial Version
+- Contains code generation core functionality and SimpleMetadata model for databases
